dace1749f18fc6f43b8be44f8a5518b8d55dedf8
theprashantbhagat <bhagatprashant1997@gmail.com>
2024-12-11 23:20:03 +0530

    hgj

---
+++ src/main/java/com/sud/estatement/EstatementApplication.java
@@ -0,0 +1,13 @@
+package com.sud.estatement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstatementApplication.class, args);
+	}
+
+}
